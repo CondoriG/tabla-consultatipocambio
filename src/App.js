da212b309df4707d14ea6d2210906c264d448640
Renzito <krcondori@gmail.com>
2019-06-01 07:39:04 -0500

    creacion tablita

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      post: []
+    }
+  }
+
+
+  componentDidMount(){
+    const url = "https://api-dolares.herokuapp.com/api/anio/2015";
+    fetch(url, {
+      method: "GET"
+    }).then(response => response.json()).then(posts => {
+      this.setState({posts: posts})
+    })
+  }
+
+  render(){
+    const columns = [
+      {
+        Header: "Fecha",
+        accessor: "fecha",
+        style:{
+          textAlign: "center"
+        },
+        width: 100,
+        maxWidth: 100,
+        minWidth:100
+      },
+      {
+        Header: "Importe",
+        accessor: "importe",
+        style:{
+          textAlign: "center"
+        },
+        width: 100,
+        maxWidth: 100,
+        minWidth:100
+      }
+    ]
+    return(
+      <div>
+        <ReactTable
+        columns={columns}
+        data={this.state.posts}
+      />
+      </div>
+      
+    );
+  }
+
 }
 
 export default App;
